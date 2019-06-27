9bcdd3cb03c8fc079ec218ae431d85b27a039517
Peacess <peacess@163.com>
2019-06-27 09:06:39 +0800

    two

---
+++ app/app/app.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"github.com/scryinfo/dot/dot"
+	"github.com/scryinfo/dot/dots/gindot"
+	"github.com/scryinfo/dot/dots/line"
+	"github.com/scryinfo/scryg/sutils/sfile"
+	"github.com/scryinfo/scryg/sutils/ssignal"
+	"go.uber.org/zap"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	l, err := line.BuildAndStart(Add)
+	if err != nil {
+		dot.Logger().Errorln("", zap.Error(err))
+		return
+	}
+	defer line.StopAndDestroy(l, true)
+	dot.Logger().Infoln("dot ok")
+
+	ssignal.WaitCtrlC(func(s os.Signal) bool {
+		return false
+	})
+	dot.Logger().Infoln("dot will stop")
+}
+
+//AddNewers add newers of api
+func Add(l dot.Line) error {
+	//add the controller
+	lives := gindot.TypeLiveGinDot()
+	err := l.PreAdd(lives)
+
+	l.ToDotEventer().ReSetLiveEvents(dot.LiveId(gindot.EngineLiveId), &dot.LiveEvents{
+		AfterCreate: func(live *dot.Live, l dot.Line) {
+			if engine, ok := live.Dot.(*gindot.Engine); ok {
+				ex := "."
+				{
+					et, err := os.Executable()
+					if err == nil {
+						ex = filepath.Dir(et)
+						if !sfile.ExistFile(filepath.Join(ex, "dist")) {
+							temp := filepath.Dir(ex)
+							temp = filepath.Dir(temp)
+							if sfile.ExistFile(filepath.Join(temp, "app/dist")) {
+								ex = temp
+							}
+						}
+					}
+				}
+				engine.GinEngine().Static("/", ex)
+			}
+		},
+	})
+	return err
+}
--- services/apply.go
+++ services/apply.go
@@ -4,41 +4,16 @@ import (
 	"github.com/scryinfo/apply/dots/auth2"
 	"github.com/scryinfo/apply/services/rapi"
 	"github.com/scryinfo/dot/dot"
-	"github.com/scryinfo/dot/dots/gindot"
 	"github.com/scryinfo/dot/dots/grpc/gserver"
-	"github.com/scryinfo/scryg/sutils/sfile"
-	"os"
-	"path/filepath"
 )
 
 //AddNewers add newers of api
 func Add(l dot.Line) error {
 	//add the controller
-	lives := gserver.GinNoblTypeLives()
+	lives := gserver.HttpNoblTypeLives()
 	lives = append(lives, auth2.Auth2TypeLives()...)
 	lives = append(lives, rapi.RapiServerTypeLives()...)
 	err := l.PreAdd(lives...)
 
-	l.ToDotEventer().ReSetLiveEvents(dot.LiveId(gindot.EngineLiveId), &dot.LiveEvents{
-		AfterCreate: func(live *dot.Live, l dot.Line) {
-			if engine, ok := live.Dot.(*gindot.Engine); ok {
-				ex := "."
-				{
-					et, err := os.Executable()
-					if err == nil {
-						ex = filepath.Dir(et)
-						if !sfile.ExistFile(filepath.Join(ex, "dist")) {
-							temp := filepath.Dir(ex)
-							temp = filepath.Dir(temp)
-							if sfile.ExistFile(filepath.Join(temp, "app/dist")) {
-								ex = temp
-							}
-						}
-					}
-				}
-				engine.GinEngine().Static("/app", filepath.Join(ex, "app"))
-			}
-		},
-	})
 	return err
 }
